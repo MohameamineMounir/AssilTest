165292f818d7316b79d5b695fe015a7d70c662ff
Mohamed-amine-Mr <mohamedaminemounirdev@gmail.com>
2024-12-06 11:57:48 +0100

    improve styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="bg-green-800 text-white text-center p-4 font-tajawal">
-      <p>© 2024 مطبخ المغرب - نكهات أصيلة</p>
+      <p>© 2024 أصيل</p>
     </footer>
   );
 };
