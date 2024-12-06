4520003f3698cf762dfa7cb013e1b9be449d5238
Mohamed-amine-Mr <mohamedaminemounirdev@gmail.com>
2024-12-06 14:59:00 +0100

    add imges

--- src/components/DishesPage.jsx
+++ src/components/DishesPage.jsx
@@ -18,6 +18,8 @@ import saladevaries from "../assets/saladevaries.jpg";
 import kouskous from "../assets/kouskous.jpg";
 import tajine from "../assets/tajine.jpg";
 import Fruits from "../assets/fruits.jpg";
+import { FaWhatsapp } from "react-icons/fa";
+
 const DishesPage = () => {
   const productCategories = [
     {
@@ -128,74 +130,82 @@ const DishesPage = () => {
   ];
 
   return (
-    <section
-      id="dishes"
-      className="container mx-auto my-16 px-4 rtl font-tajawal text-center"
-    >
-      {productCategories.map((category, categoryIndex) => (
-        <div key={categoryIndex} className="mb-16">
-          <h2 className="text-3xl font-extrabold text-green-700 text-center mb-10">
-            {category.category}
-          </h2>
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-8">
-            {category.items.map((dish, dishIndex) => (
-              // <div
-              //   key={dishIndex}
-              //   className="bg-white rounded-xl shadow-lg overflow-hidden transition-transform duration-300 hover:scale-105 hover:shadow-2xl"
-              // >
-              //   <div className="relative">
-              //     <img
-              //       src={dish.image}
-              //       alt={dish.name}
-              //       className="w-full h-52 object-cover rounded-t-xl"
-              //     />
-              //     <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent opacity-75 rounded-t-xl"></div>
-              //     <h3 className="absolute inset-0 flex items-center justify-center text-xl font-semibold text-white">
-              //       {dish.name}
-              //     </h3>
-              //   </div>
+    <>
+      <section
+        id="dishes"
+        className="container mx-auto my-16 px-4 rtl font-tajawal text-center"
+      >
+        {productCategories.map((category, categoryIndex) => (
+          <div key={categoryIndex} className="mb-16">
+            <h2 className="text-3xl font-extrabold text-green-700 text-center mb-10">
+              {category.category}
+            </h2>
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-8">
+              {category.items.map((dish, dishIndex) => (
+                // <div
+                //   key={dishIndex}
+                //   className="bg-white rounded-xl shadow-lg overflow-hidden transition-transform duration-300 hover:scale-105 hover:shadow-2xl"
+                // >
+                //   <div className="relative">
+                //     <img
+                //       src={dish.image}
+                //       alt={dish.name}
+                //       className="w-full h-52 object-cover rounded-t-xl"
+                //     />
+                //     <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent opacity-75 rounded-t-xl"></div>
+                //     <h3 className="absolute inset-0 flex items-center justify-center text-xl font-semibold text-white">
+                //       {dish.name}
+                //     </h3>
+                //   </div>
 
-              //   <div className="p-6">
-              //     <p className="text-gray-600">{dish.description}</p>
-              //     <button className="mt-4 w-full bg-green-500 text-white py-2 rounded-md font-medium hover:bg-green-600 transition">
-              //       Order Now
-              //     </button>
-              //   </div>
-              // </div>
-              <div
-                key={dishIndex}
-                className="bg-white rounded-xl shadow-lg overflow-hidden transition-transform duration-300 hover:scale-105 hover:shadow-2xl"
-              >
-                <div className="relative group">
-                  {/* Image Section */}
-                  <img
-                    src={dish.image}
-                    alt={dish.name}
-                    className="w-full h-52 object-cover rounded-t-xl"
-                  />
+                //   <div className="p-6">
+                //     <p className="text-gray-600">{dish.description}</p>
+                //     <button className="mt-4 w-full bg-green-500 text-white py-2 rounded-md font-medium hover:bg-green-600 transition">
+                //       Order Now
+                //     </button>
+                //   </div>
+                // </div>
+                <div
+                  key={dishIndex}
+                  className="bg-white rounded-xl shadow-lg overflow-hidden transition-transform duration-300 hover:scale-105 hover:shadow-2xl"
+                >
+                  <div className="relative group">
+                    {/* Image Section */}
+                    <img
+                      src={dish.image}
+                      alt={dish.name}
+                      className="w-full h-52 object-cover rounded-t-xl"
+                    />
 
-                  {/* Text Overlay */}
-                  <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent group-hover:from-green-800 group-hover:via-green-400 opacity-75 rounded-t-xl transition-all duration-300"></div>
-                  <h3 className="absolute inset-0 flex flex-col items-center justify-center text-center text-lg md:text-xl font-bold text-white px-4">
-                    {dish.name}
-                  </h3>
+                    {/* Text Overlay */}
+                    <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent group-hover:from-green-800 group-hover:via-green-400 opacity-75 rounded-t-xl transition-all duration-300"></div>
+                    <h3 className="absolute inset-0 flex flex-col items-center justify-center text-center text-lg md:text-xl font-bold text-white px-4">
+                      {dish.name}
+                    </h3>
+                  </div>
+
+                  {/* Content Section */}
+                  <div className="p-6">
+                    <p className="text-gray-700 leading-relaxed">
+                      {dish.description}
+                    </p>{" "}
+                  </div>
                 </div>
-
-                {/* Content Section */}
-                <div className="p-6">
-                  <p className="text-gray-700 leading-relaxed">
-                    {dish.description}
-                  </p>
-                  <button className="mt-4 w-full bg-green-600 text-white py-2 rounded-lg font-medium hover:bg-green-700 transition">
-                    Order Now
-                  </button>
-                </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
-    </section>
+        ))}
+      </section>
+      <a
+        href="https://wa.me/+212648614441"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="fixed bottom-6 right-6 bg-green-600 text-white p-4 rounded-full shadow-xl hover:bg-green-700 transition-colors"
+        aria-label="Contactez-nous sur Whatsapp"
+      >
+        <FaWhatsapp size={24} />
+      </a>
+    </>
   );
 };
 
