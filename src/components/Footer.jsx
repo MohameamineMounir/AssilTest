414e8419538d940d42a874e1e157f4df42635380
Mohamed-amine-Mr <mohamedaminemounirdev@gmail.com>
2024-12-07 16:18:55 +0100

    improve categories

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="bg-green-800 text-white text-center p-4 font-tajawal">
-      <p>© 2024 أصيل</p>
+      <p>© 2024 أصيل | الخضروات الجاهزة للطهي</p>
     </footer>
   );
 };
